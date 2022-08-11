7f446f72f7bd498a50790373dcf2cc4871c018ef
mayankmurari <mayank.murari@gmail.com>
2022-08-11 20:50:34 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 
 def Weather(city):
-    api_address = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=907cc9f7f1e2fb5ae0b24ec2af1d8206'
+    api_address = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid={appid}'
     #city = input('Citi Name :')
     url = api_address
     json_data = requests.get(url).json()
